30da811468a9e50cc9fb72268782fc32fca7a556
saminda <scbgamage@gmail.com>
2025-09-29 09:54:17 +0000

    navibar eke border ain kranna, and opacity tawa adu kranna, hodatama nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
   const userAvatar = PlaceHolderImages.find((img) => img.id === 'avatar-4');
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full bg-background/80 backdrop-blur-lg supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
