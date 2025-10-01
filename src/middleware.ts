abbcd1dfd153daea6ecd6c7f1958ae176bdf508a
saminda <scbgamage@gmail.com>
2025-10-01 12:37:41 +0000

    Try fixing this error: `Console Error: ClientFetchError: Failed to execu

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
-const { auth } = NextAuth(authConfig);
-export default auth;
+export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
 };
