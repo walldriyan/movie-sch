ac8ef80f7beb32560e1c97b620459b843fc76bed
saminda <scbgamage@gmail.com>
2025-10-01 12:48:23 +0000

    ko error eke lable ekaka pennanne nane, full error eke data ekka pennann

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api/register|_next/static|_next/image|favicon.ico).*)'],
 };
