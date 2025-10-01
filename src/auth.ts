f4366a652d088013a2a12e73a5ecb46ddeabd880
saminda <scbgamage@gmail.com>
2025-10-01 13:32:31 +0000

    {   "message": "MissingCSRF",   "stack": "N/A",   "name": "MissingCSRF"

--- src/auth.ts
+++ src/auth.ts
@@ -64,7 +64,6 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  trustHost: true,
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
