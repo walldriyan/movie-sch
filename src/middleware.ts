800ed7a460b7134bb9bca86c3fc037b821d6419d
saminda <scbgamage@gmail.com>
2025-10-01 13:19:03 +0000

    ClientFetchError: Failed to execute 'json' on 'Response': Unexpected end

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export default auth;
 // Optionally, don't invoke Middleware on some paths
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: ['/((?!api/register|_next/static|_next/image|favicon.ico|.*\\.png$).*)'],
+  matcher: ['/((?!api/auth|api/register|_next/static|_next/image|favicon.ico|.*\\.png$).*)'],
 };
