ad8cd63902e789cc94393558c5608876ab748556
saminda <scbgamage@gmail.com>
2025-10-01 09:46:34 +0000

    mewage container border eka 2xl tiyanna, button wala full rounderd wenna

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,9 +74,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: '9999px',
-        md: '9999px',
-        sm: '9999px',
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
         'accordion-down': {
