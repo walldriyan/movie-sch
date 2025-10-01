2738beb315ce2615c346bfbabaa2c8a9e36f33c4
saminda <scbgamage@gmail.com>
2025-10-01 12:33:11 +0000

    Try fixing this error: `Console Error: ClientFetchError: Failed to execu

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
 import NextAuth from 'next-auth';
 import { authOptions } from './app/api/auth/[...nextauth]/route';
 
-const { auth } = NextAuth(authOptions as any);
-
-export default auth;
+export default NextAuth(authOptions as any).auth;
 
 export const config = {
   matcher: ['/manage/:path*', '/profile/:path*'],
