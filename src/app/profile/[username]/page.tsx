7ec5e9677dd81ea467380b486581d055ab7767fd
saminda <scbgamage@gmail.com>
2025-10-03 22:21:40 +0000

    dan mokakda me aula?

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -40,7 +40,7 @@ export default async function ProfilePage({
   return (
     <>
       <ProfileHeader user={profileUser} currentFilter={currentFilter} isOwnProfile={isOwnProfile}/>
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-red-900">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div className="md:col-span-2">
             <ProfilePostList
