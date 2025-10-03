dc6a9bfc4429eb4579611469bcaea679bdaf78a2
saminda <scbgamage@gmail.com>
2025-10-03 08:37:52 +0000

    me div eke atule eliment lfet ekata align kranna padin left ain karala (

--- src/app/movies/[id]/movie-detail-client.tsx
+++ src/app/movies/[id]/movie-detail-client.tsx
@@ -146,7 +146,7 @@ export default function MovieDetailClient({
             ))}
         </div>
 
-        <div className="relative z-10 text-foreground flex flex-col items-start text-left pb-0 w-full px-8">
+        <div className="relative z-10 text-foreground flex flex-col items-start text-left pb-0 w-full pr-8">
           <h1 className="font-serif text-3xl md:text-5xl font-bold leading-tight mb-4 text-left">
             {movie.title}
           </h1>
