d44731ca77b297a169af7d39e08f99258c104a5f
saminda <scbgamage@gmail.com>
2025-10-04 09:13:33 +0000

    tawamat ehemamai notificatin menu eka show unama navi bar ekai anik sama

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -128,7 +128,7 @@ export default async function Header({
   };
 
   return (
-    <header className="sticky top-0 z-40 w-full bg-background/80 backdrop-blur-lg border-b border-white/10">
+    <header className="sticky top-0 z-50 w-full bg-background/80 backdrop-blur-lg border-b border-white/10">
       <div className="px-4 flex h-16 items-center justify-between gap-8">
         {children || (
           <div className="flex items-center gap-4">
