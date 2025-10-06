43d64df8f837f990ea31a59832e7d9c902ce3dfd
saminda <scbgamage@gmail.com>
2025-10-06 23:06:09 +0000

    log wenakota user name and password dekama db eke tiyanwada chk krala lo

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -67,12 +67,12 @@ export async function authenticate(
   formData: FormData
 ) {
   try {
-    await signIn('credentials', formData);
+    await signIn('credentials', { ...Object.fromEntries(formData), redirectTo: '/' });
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
         case 'CredentialsSignin':
-          return 'Invalid credentials.';
+          return 'Invalid email or password.';
         default:
           return 'Something went wrong.';
       }
@@ -82,7 +82,7 @@ export async function authenticate(
 }
 
 export async function doSignOut() {
-  await signOut();
+  await signOut({ redirectTo: '/' });
 }
 
 export async function registerUser(
