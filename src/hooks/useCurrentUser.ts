49f211625dacce6613c789aba609fea64cc92d7b
saminda <scbgamage@gmail.com>
2025-10-14 12:23:48 +0000

    sesson walata amatarawa use curent user ge datat console log kranna , lo

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -6,5 +6,6 @@ import { useSession } from "next-auth/react";
 export const useCurrentUser = () => {
   const { data: session } = useSession();
   console.log('[useCurrentUser] hook session data:', session);
+  console.log('[useCurrentUser] hook user data:', session?.user);
   return session?.user;
 };
