1a18b2facf7369b3e705cae443140ec78b2c2b04
saminda <scbgamage@gmail.com>
2025-10-16 11:31:29 +0000

    Invalid `prisma.user.update()` invocation: 2025-10-16T11:30:19Z [web] 2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
+// add prisma to the NodeJS global type
 declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined
 }
 
-const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log:
+// Prevent multiple instances of Prisma Client in development
+const prisma = global.prisma ?? new PrismaClient({
+  log:
       process.env.NODE_ENV === 'development'
         ? ['query', 'error', 'warn']
         : ['error'],
-  });
+});
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
