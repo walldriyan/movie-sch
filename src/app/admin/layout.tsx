8d95a400f393d5ebc6c0ccbc4f4d326480d6461e
saminda <scbgamage@gmail.com>
2025-10-21 22:20:43 +0000

    magine top eka ain kranna meka atule pading 5 px tiyanna e ati (_for ele

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -37,7 +37,7 @@ export default function AdminLayout({
   }
 
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 mt-16">
+    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 pt-5">
       {children}
     </main>
   );
