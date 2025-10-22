7104784c8c005c409b52814baf015829555590bf
saminda <scbgamage@gmail.com>
2025-10-22 14:57:56 +0000

    Try fixing this error: `Console ReferenceError: useState is not defined.

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useEffect, useTransition } from 'react';
+import { useEffect, useTransition, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
