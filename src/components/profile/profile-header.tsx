aec5927b428fdc7bad0514547193a871aef64403
saminda <scbgamage@gmail.com>
2025-10-31 14:58:59 +0000

    meka 3xl round krala margine 10px allside danna (_for element <ProfileHe

--- src/components/profile/profile-header.tsx
+++ src/components/profile/profile-header.tsx
@@ -22,18 +22,18 @@ export default function ProfileHeader({ user, currentFilter, isOwnProfile }: { u
 
   return (
     <div className="border-b bg-background overflow-hidden ">
-      <div className="relative h-48 group">
+      <div className="relative h-48 group p-2.5">
         {coverImage && (
             <Image
                 src={coverImage}
                 alt="Cover image"
                 fill
-                className="object-cover"
+                className="object-cover rounded-3xl"
             />
         )}
         <div className="absolute inset-0 bg-gradient-to-t from-background via-background/50 to-transparent" />
         {isOwnProfile && (
-           <div className="absolute inset-0 bg-black/40 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+           <div className="absolute inset-2.5 bg-black/40 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity rounded-3xl">
               <EditProfileDialog
                   user={user}
                   triggerButton={
