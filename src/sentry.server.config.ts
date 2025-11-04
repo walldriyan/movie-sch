3d3176b15384eff052ca5f30b0571b00e5887996
saminda <scbgamage@gmail.com>
2025-11-04 11:44:55 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/sentry.server.config.ts
+++ src/sentry.server.config.ts
@@ -2,13 +2,17 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import { init } from "@sentry/nextjs";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 
-Sentry.init({
-  dsn: "https://d1a262abc902034babd62ceae0eaa12e@o4510267385577472.ingest.us.sentry.io/4510267387740160",
+init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+
+  // Define how likely profiles are sampled.
+  profilesSampleRate: 1.0,
 
   // Enable logs to be sent to Sentry
   enableLogs: true,
@@ -18,5 +22,7 @@ Sentry.init({
   sendDefaultPii: true,
   
   // Disable default integrations to prevent build errors with dynamic imports
-  integrations: [],
+  integrations: [
+    new ProfilingIntegration(),
+  ],
 });
